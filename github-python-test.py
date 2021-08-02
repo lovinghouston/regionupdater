ba4befa77077c1883a5403d04390d55b5eb80cca
Julian Joseph <Julian88Tex@gmail.com>
2021-08-01 20:43:12 -0700

    small coordinate change to test

--- github-python-test.py
+++ github-python-test.py
@@ -9,7 +9,7 @@ import csv
 
 # HoustonISD = gpd.read_file("C:/Users/crazy/OneDrive/Documents/VS Code Stuff/HighAttendanceZones1920/Original/HighAttendanceZones1920.shp")
 
-lati,long = 29.693211, -95.272556 #29.737586775000725, -95.40166115040519 #29.735600, -95.328620
+lati,long = 39.693211, -95.272556 #29.737586775000725, -95.40166115040519 #29.735600, -95.328620
 CurPoint = Point(Transformer.from_crs("epsg:4326", "epsg:2278").transform(lati,long))
 
 # print(HoustonISD.loc[0][1])
