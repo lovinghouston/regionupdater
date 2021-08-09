e1161c51c003de2b2ec9df2dae1a2360b4bcfc68
Jonathan Y <jonye.xdd@gmail.com>
2021-08-09 00:48:55 -0700

    Add request token query, new comments, cleaned some temporary code

--- regionUpdaterCode.py
+++ regionUpdaterCode.py
@@ -9,32 +9,43 @@ import json
 import csv
 import requests #install
 
-#get terminal text if it fails
-#return that as error in the tracker (change the name of the CSV too)
 #figure out what happened with that Kansas one
 
 HoustonISD = gpd.read_file("shape/HoustonISD/HighAttendanceZones1920.shp")
 TexasDistricts = gpd.read_file("shape/TexasSchoolDistricts/SchoolDistricts_2021.shp")
 
 alldistricts = [] #TEMPORARY
+#ITERATIONS_END = 2000 #For testing - ends iterations early
 churches = []
 logResults = []
 logResults.append(["Number","Church Account ID","Church Name","Region Update Input","API Response Text","Error"])
+
+#Retrieve Access Token
+url = {{AUTH_URL}} #put information to get token
+payload = ""
+headers = {
+  'Cookie': 'BrowserId=L1U6KxlzEeuszK_80mI4gA; CookieConsentPolicy=0:0'
+}
+response = requests.request("POST", url, headers=headers, data=payload)
+tokenTemp = response.text.replace('"',"").split(',')
+ttype = tokenTemp[3].split(':')[1]
+token = "" + ttype + " " + tokenTemp[0].split(':')[1] + ""
 
 #url query to get Church data
 #Note: Changed date to 7200 for testing purposes
 url = "https://lovinghouston--partial.my.salesforce.com/services/data/v40.0/query/?q=SELECT+Id,Name,BillingLongitude,BillingLatitude+FROM+Account+WHERE+Type+=+'Church'+AND+BillingLongitude+!=+null+AND+Houston_ISD_Region__c+=+null+AND+CreatedDate+=+LAST_N_DAYS:7200"
 headers = {
-    'Authorization': 'Bearer 00D6t0000008fyq!AR0AQKcs00wO13v5c_D_RugWA0omMUm8RfMXiS8WXASQarHEknZaFkz3SY4oX5QPl5njdimU7HZuVSGwKTFvQ0IIZwhW6P8_'
+    'Authorization': token
 }
 response = requests.request("GET", url, headers=headers)
 reader = json.loads(response.text)
 
-#with open("./Json query test.json", "w+") as jsonfile:
-#    jsonfile.write(response.text)
-#    reader = json.load(jsonfile)
+#Check if token is expired and quit program if so
+if response.text == '[{"message":"Session expired or invalid","errorCode":"INVALID_SESSION_ID"}]':
+    print(response.text)
+    exit()
 
-#Read church JSON
+#Read churches query into churches list, define indices and format 
 for i in range(len(reader["records"])):
     tempID = reader["records"][i]["Id"]
     templong = reader["records"][i]["BillingLongitude"]
@@ -44,6 +55,7 @@ for i in range(len(reader["records"])):
     IDIndex, NameIndex, LongIndex, LatIndex = 0,1,2,3
     churches.append(row)
 
+#Switch function to parse area numbers into Houston ISD regions
 def RegionSwitch(argument):
     switcher = {
     0: "East",
@@ -72,18 +84,19 @@ def RegionSwitch(argument):
     }
     return switcher.get(argument, "Not in Houston ISD polygons")
 
+#Switch function to parse district names into Salesforce Account IDs
 def DistrictSwitch(argument): #NOT DONE
     switcher = {
-#    'Columbus ISD': "",
+    #'Columbus ISD': "",
     'Cypress-Fairbanks ISD': "0011N000018j7K6QAI",
-#    'Spring Branch ISD': "0011N000018iy7eQAA",
+    #'Spring Branch ISD': "0011N000018iy7eQAA",
     'Aldine ISD': "0011N000018j7KNQAY",
     'Tomball ISD': "0011N000018j7KCQAY",
     'Pasadena ISD': "0011N000018j7K9QAI",
-#    'Dickinson ISD': "",
+    #'Dickinson ISD': "",
     'New Caney ISD': "0011N000018j7K2QAI",
     'Sheldon ISD': "0013l00002Hm1BrAAJ",
-#    'Lamar Cons ISD': "0011N000018iyBMQAY",
+    #'Lamar Cons ISD': "0011N000018iyBMQAY",
     'Katy ISD': "0011N000018j7K8QAI",
     'Spring ISD': "0011N000018j7K1QAI",
     'Galena Park ISD': "0011N000018j7KKQAY",
@@ -91,15 +104,16 @@ def DistrictSwitch(argument): #NOT DONE
     'Brazosport ISD': "0011N00001VYDr2QAH",
     'Humble ISD': "0011N000018j7KEQAY",
     'Pearland ISD': "0011N000018j7KHQAY",
-#    'Beaumont ISD': "",
-#    'Fort Bend ISD': "0011N000018iyQhQAI",
+    #'Beaumont ISD': "",
+    #'Fort Bend ISD': "0011N000018iyQhQAI",
     'Clear Creek ISD': "0011N000018j7K4QAI",
-#    'Klein ISD': "0011N000018iyCRQAY",
+    #'Klein ISD': "0011N000018iyCRQAY",
     'Alief ISD': "0011N000018j7K3QAI",
-#    'Stafford MSD': "",
+    #'Stafford MSD': "",
     }
     return switcher.get(argument, "")
 
+#Function to compare point against Houston ISD areas polygons
 def Locate(church,point):
     region = ""
     for reg in range(len(HoustonISD)):
@@ -107,6 +121,7 @@ def Locate(church,point):
             region = '{"Houston_ISD_Region__c": "' + RegionSwitch(reg) + '", "ParentID": "0016t00000AmYsRAAV"}'
     return region
 
+#Function to compare point against Texas School Districts polygons
 def DistLocate(church,point):
     district = ""
     for dist in range(len(TexasDistricts)):
@@ -115,6 +130,8 @@ def DistLocate(church,point):
 #            alldistricts.append(TexasDistricts.loc[dist]['NAME'])
     return district
 
+#Iterate through each church, transforms lat/long from Google Maps map system into Houston ISD or Texas Districts system
+#Then, uploads result to Salesforce
 for i in range(len(churches)):
     anyError = ""
     CurChurch = churches[i][NameIndex]
@@ -131,22 +148,24 @@ for i in range(len(churches)):
     url = "https://lovinghouston--partial.my.salesforce.com/services/data/v50.0/sobjects/Account/" + str(churches[i][IDIndex])
     headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer 00D6t0000008fyq!AR0AQKcs00wO13v5c_D_RugWA0omMUm8RfMXiS8WXASQarHEknZaFkz3SY4oX5QPl5njdimU7HZuVSGwKTFvQ0IIZwhW6P8_'
+        'Authorization': token
     }
     response = requests.request("PATCH", url, headers=headers, data=result)
-
 #    print(response.text)
 
+#Collect data (number, Account ID, Church Name, Data sent to Salesforce, response from API, Errors input here) into a line to log
     logLine = [i,churches[i][IDIndex],CurChurch,result,response.text,anyError]
     logResults.append(logLine)
 
-    if i>20:
+#Temporary testing tool to cull number iterations early
+    if i>{{ITERATIONS_END}}:
         break
 
+#Enter logged data into CSV
 with open('RegionUpdaterResultsTracker.csv', 'w', newline='') as csvfile:
     csvwriter = csv.writer(csvfile)
     csvwriter.writerows(logResults)
 
-
+#   Along with alldistricts definition above in DistLocate function, this provides list of all identified districts where churches are located
 #alldistricts = list(set(alldistricts))
 #print(alldistricts)
